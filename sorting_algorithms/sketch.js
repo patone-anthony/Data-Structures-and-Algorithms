9449d09dc64bb84d3f8d68673f4ad387113803f7
patone-anthony <48881875+patone-anthony@users.noreply.github.com>
2020-02-17 21:36:29 -0500

    Add files via upload

---
+++ sorting_algorithms/sketch.js
@@ -0,0 +1,119 @@
+let values = [];
+let w = 5;
+var cnv;
+
+let states = [];
+
+function setup() {
+  cnv = createCanvas(windowWidth * 0.9, windowHeight / 2);
+  var x = (windowWidth - width) / 2;
+  var y = 200;
+
+  centerCanvas();
+
+  values = new Array(floor(width / w));
+  for (let i = 0; i < values.length; i++) {
+    values[i] = random(0, height);
+    states[i] = -1;
+  }
+  quickSort(values, 0, values.length - 1);
+}
+
+function centerCanvas() {
+  var xaxis = (windowWidth - width) / 2;
+  var yaxis = (windowHeight - height) / 2;
+  cnv.position(xaxis, yaxis);
+}
+
+function windowResized() {
+  setup();
+  resizeCanvas(windowWidth * 0.9, windowHeight / 2);
+
+  centerCanvas();
+}
+
+canvas.parent("sketch-container");
+// end formatting
+
+function draw() {
+  background(250, 250, 250);
+
+  for (let i = 0; i < values.length; i++) {
+    noStroke();
+    if (states[i] == 0) {
+      fill("#E0777D");
+    } else if (states[i] == 1) {
+      fill("#D6FFB7");
+    } else {
+      fill(43, 43, 43);
+    }
+    rect(i * w, height - values[i], w, values[i]);
+  }
+}
+
+async function quickSort(arr, start, end) {
+  if (start >= end) {
+    return;
+  }
+  let index = await partition(arr, start, end);
+  states[index] = -1;
+
+  await Promise.all([
+    quickSort(arr, start, index - 1),
+    quickSort(arr, index + 1, end)
+  ]);
+}
+
+async function partition(arr, start, end) {
+  for (let i = start; i < end; i++) {
+    states[i] = 1;
+  }
+
+  let pivotValue = arr[end];
+  let pivotIndex = start;
+  states[pivotIndex] = 0;
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivotValue) {
+      await swap(arr, i, pivotIndex);
+      states[pivotIndex] = -1;
+      pivotIndex++;
+      states[pivotIndex] = 0;
+    }
+  }
+  await swap(arr, pivotIndex, end);
+
+  for (let i = start; i < end; i++) {
+    if (i != pivotIndex) {
+      states[i] = -1;
+    }
+  }
+
+  return pivotIndex;
+}
+
+function draw() {
+  background(250, 250, 250);
+
+  for (let i = 0; i < values.length; i++) {
+    noStroke();
+    if (states[i] == 0) {
+      fill("#db232e");
+    } else if (states[i] == 1) {
+      fill("#4cff4c");
+    } else {
+      fill(31, 31, 31);
+    }
+    rect(i * w, height - values[i], w, values[i]);
+  }
+}
+
+async function swap(arr, a, b) {
+  await sleep(25);
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
